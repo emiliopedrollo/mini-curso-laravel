82f9ae63433ba339fc30d13d71b0ff5955e930ee
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2019-04-07 18:33:21 -0300

    Add Hello world example for console

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+Artisan::command('hello {name}', function() {
+    $this->info("Hello " . $this->argument('name'));
+});
