56a97b2bbf9cdf0d727d40cab49135d9fc8a4bd8
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2019-04-20 21:42:19 -0300

    Add some routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,50 @@ Route::get('/', function () {
 Route::get('/hello/{name}', function ($name) {
     return "Hello ".$name;
 });
+
+Route::post('/hello/{name}', function (\Illuminate\Http\Request $request, $name) {
+    return "Hello ".$name."\nConteúdo: ".$request->getContent();
+});
+
+Route::match(['put','patch','delete'],'/hello/{name}', function (\Illuminate\Http\Request $request, $name) {
+    return "Hello ".$name.
+        "\nMétodo: ".
+        $request->method().
+        "\nConteúdo: ".
+        $request->getContent();
+});
+
+Route::get('/final', function() {
+    return "This is your final destination!!";
+});
+
+Route::redirect("/old_path","/final");
+
+Route::prefix('post')->group(function() {
+
+    // 123e4567-e89b-12d3-a456-426655440000
+    Route::get('/{uuid}', function ($uuid) {
+        return "Post with uuid $uuid";
+    })->where(['uuid' => '^[0-9a-fA-F]{8}(\-[0-9a-fA-F]{4}){3}\-[0-9a-fA-F]{12}$']);
+
+    Route::get('/{id}', function ($id) {
+        return "Post with id $id";
+    })->where(['id' => '[0-9]+']);
+
+    Route::get('/{slug}', function ($slug) {
+        return "Post with slug $slug";
+    })->where(['slug' => '[A-Za-z0-9\-]+']);
+
+});
+
+Route::get('/a-route', function(){
+    return now();
+})->name('now');
+
+Route::get('/where-is-now', function(){
+    return route('now');
+});
+
+Route::get('/redirect-me-to-now', function() {
+    return redirect()->route('now');
+});
